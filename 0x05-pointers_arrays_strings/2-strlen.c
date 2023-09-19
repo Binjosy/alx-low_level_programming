841d57a528a61c0e9ebadd7dc05c0f325cd3ffd7
Binjosy <140069368+Binjosy@users.noreply.github>
2023-09-19 08:41:53 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
+#include "main.h"
 /**
  * _strlen - returns the length of a string
  * @s: string
- * return: length
+ * Return: length
  */
 int _strlen(char *s)
 {
 	int longi = 0;
 
 	while (*s != '\0')
-
 	{
 		longi++;
 		s++;
